44ad722d66b1cc7fe36583f1c806bf196731de3d
SandipDotball <sandip.pal18@gmail.com>
2021-05-23 20:30:30 +0530

    Add section in home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import dynamic from 'next/dynamic'
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import WrapperApp from '../containers/Wrapper/App'
 import GlobalStyle from '../globalStyle'
+
 // const Provider = dynamic(() => import('react-redux').then(mod => mod.Provider))
 import { store } from '../store'
 function MyApp (props) {
---
+++ pages/home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import HomePage from '../containers/Pages/Home'
+
+function home () {
+  return <HomePage />
+}
+
+export default home
